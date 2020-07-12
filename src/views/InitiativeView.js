a7b9b9fa019d4159fd3cb50cfc0c6e49a461e395
Franco Mangone <francomangone18@gmail.com>
2020-07-12 13:05:17 -0300

    Started using leaflet

--- src/views/InitiativeView.js
+++ src/views/InitiativeView.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
 
 /* Helpers */
 import { getCategoryDisplay } from '../helpers/getCategoryDisplay';
@@ -8,7 +10,7 @@ import { getCategoryDisplay } from '../helpers/getCategoryDisplay';
 import '../styles/initiative.scss';
 
 
-const InitiativeView = () => {
+const InitiativeView = (props) => {
 
 	let { id } = useParams();
 	const [initiative, setInitiative] = useState(null);
@@ -27,17 +29,37 @@ const InitiativeView = () => {
 		let { name, category, hood, city, _id } = initiative;
 		let { categoryDisplay, icon } = getCategoryDisplay(category);
 
+		// Dummy map default props
+		let defaultProps = {
+		    center: {
+		      lat: -34.878388,
+		      lng: -56.250416
+		    },
+		    zoom: 11
+		  };
+
 		return (
 			<div className='initiative-jumbotron-wrapper'>
 				<div className='details'>
 					<h1 className='name'>{name}</h1>
-					<div className='category-wrapper'>
+					<div className='info-wrapper'>
 						<p className='icon'>{icon}</p>
-						<p className='category'>{categoryDisplay}</p>
+						<p className='info'>{`${categoryDisplay} - ${hood}, ${city}`}</p>
+						<p className='location'></p>
 					</div>
-					<p className='location'>{`${hood}, ${city}`}</p>
 				</div>
-				<div className='map'></div>
+				<div className='map-wrapper'>
+					<Map center={[-34.878388, -56.250416]} zoom={15}>
+				    	<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				        		   attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
+				        <Marker key={0}
+						        position={[
+						        	-34.878388,
+						            -56.250416
+						        ]}
+						        onClick={() => {}} />
+				    </Map>
+				</div>
 			</div>
 		)
 	}
