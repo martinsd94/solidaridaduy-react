b2e9bcb564f6e8b1b864f0255ebc2544ea4f44b0
Franco Mangone <francomangone18@gmail.com>
2020-07-14 12:47:19 -0300

    Rearranged Initiative view

--- src/views/InitiativeView.js
+++ src/views/InitiativeView.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { FaClock, FaHome, FaPhone }  from 'react-icons/fa';
+import { FaClock, FaHome, FaPhone, FaMapMarkedAlt }  from 'react-icons/fa';
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
 
@@ -44,9 +44,10 @@ const InitiativeView = (props) => {
 				<div className='initiative-jumbotron-wrapper'>
 					<Details name={name} category={category} hood={hood} province={province}
 							 contact_phones={contact_phones} address={address} />
-					<MapContainer geolocation={geolocation} />
 				</div>
+				<MapContainer geolocation={geolocation} />
 				<Schedule activities={activities} />
+				<div className='aligner'></div>
 			</React.Fragment>
 		)
 	}
@@ -62,7 +63,13 @@ const Details = ({ name, category, hood, province, address, contact_phones }) =>
 	const { categoryDisplay, icon } = getCategoryDisplay(category);
 
 	const scrollToSchedule = () => {
-		const e = document.querySelector('.initiative-schedule');
+		//const e = document.querySelector('.initiative-schedule');
+		const e = document.getElementById('schedule-title');
+		e.scrollIntoView({ behavior: 'smooth' });
+	}
+
+	const scrollToMap = () => {
+		const e = document.getElementById('map-title');
 		e.scrollIntoView({ behavior: 'smooth' });
 	}
 
@@ -89,9 +96,14 @@ const Details = ({ name, category, hood, province, address, contact_phones }) =>
 				}
 			</div>
 			<div className='info-wrapper'>
+				<p className='icon'><FaMapMarkedAlt /></p>
+				<button className='button-link'
+						onClick={() => scrollToMap()}><p>Ver ubiación</p></button>
+			</div>
+			<div className='info-wrapper'>
 				<p className='icon'><FaClock /></p>
 				<button className='button-link'
-						onClick={() => scrollToSchedule()}><p>Ver Horarios</p></button>
+						onClick={() => scrollToSchedule()}><p>Ver horarios</p></button>
 			</div>
 		</div>
 	)
@@ -103,18 +115,21 @@ const MapContainer = ({ geolocation }) => {
 	let { latitude, longitude } = geolocation;
 
 	return (
-		<div className='map-wrapper'>
-			<Map center={[latitude, longitude]} zoom={15}>
-		    	<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-		        		   attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
-		        <Marker key={0}
-				        position={[
-				        	latitude,
-				            longitude
-				        ]}
-				        onClick={() => {}} />
-		    </Map>
-		</div>
+		<React.Fragment>
+			<h1 className='section-title' id='map-title'>Ubicación</h1>
+			<div className='map-wrapper'>
+				<Map center={[latitude, longitude]} zoom={15} scrollWheelZoom={false}>
+			    	<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			        		   attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
+			        <Marker key={0}
+					        position={[
+					        	latitude,
+					            longitude
+					        ]}
+					        onClick={() => {}} />
+			    </Map>
+			</div>
+		</React.Fragment>
 	);
 }
 
@@ -131,21 +146,23 @@ const Schedule = ({ activities }) => {
 	let { sun, mon, tue, wed, thu, fri, sat} = activities;
 
 	return (
-		<div className='initiative-schedule'>
-			<h1>Horarios</h1>
-			<div className='schedule-header'>
-				<h3>Hora</h3>
-				{ HOURS.map((hour, j) => ( <div className='hour' key={j}>{hour}</div> ))}
-			</div>
+		<React.Fragment>
+			<h1 className='section-title' id='schedule-title'>Horarios</h1>
+			<div className='initiative-schedule'>
+				<div className='schedule-header'>
+					<h3>Hora</h3>
+					{ HOURS.map((hour, j) => ( <div className='hour' key={j}>{hour}</div> ))}
+				</div>
 
-			<DaySchedule day='Domingo'   activities={sun} />
-			<DaySchedule day='Lunes'     activities={mon} />
-			<DaySchedule day='Martes'    activities={tue} />
-			<DaySchedule day='Miércoles' activities={wed} />
-			<DaySchedule day='Jueves'    activities={thu} />
-			<DaySchedule day='Viernes'   activities={fri} />
-			<DaySchedule day='Sábado'    activities={sat} />
-		</div> 
+				<DaySchedule day='Domingo'   activities={sun} />
+				<DaySchedule day='Lunes'     activities={mon} />
+				<DaySchedule day='Martes'    activities={tue} />
+				<DaySchedule day='Miércoles' activities={wed} />
+				<DaySchedule day='Jueves'    activities={thu} />
+				<DaySchedule day='Viernes'   activities={fri} />
+				<DaySchedule day='Sábado'    activities={sat} />
+			</div> 
+		</React.Fragment>
 	)
 }
 
