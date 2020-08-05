d3f8c4fa37da6aee9f365bfa82251e1815bb409d
Franco Mangone <francomangone18@gmail.com>
2020-08-05 19:07:39 -0300

    Working on F007

--- src/views/InitiativeView.js
+++ src/views/InitiativeView.js
@@ -1,235 +1,301 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { FaClock, FaHome, FaPhone, FaMapMarkedAlt, FaHandHoldingHeart }  from 'react-icons/fa';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+  FaClock,
+  FaHome,
+  FaPhone,
+  FaMapMarkedAlt,
+  FaHandHoldingHeart,
+} from "react-icons/fa";
 import { Map, Marker, TileLayer } from "react-leaflet";
 //import { Icon } from "leaflet";
 
 /* Contex */
-import { useData } from '../context/data';
+import { useData } from "../context/data";
 
 /* Helpers */
-import { getCategoryDisplay } from '../helpers/getCategoryDisplay';
+import { getCategoryDisplay } from "../helpers/getCategoryDisplay";
 
 /* Styles */
-import '../main.scss';
-import '../styles/initiative.scss';
+import "../main.scss";
+import "../styles/initiative.scss";
 
 const InitiativeView = (props) => {
+  const { id } = useParams();
+  const { data } = useData();
+  const [initiative, setInitiative] = useState(null);
 
-	const { id } = useParams();
-	const { data } = useData();
-	const [initiative, setInitiative] = useState(null);
-
-	// Fetch initiative data
-	useEffect(() => {
-		/*fetch(`http://localhost:5000/initiative/${id}`, {
+  // Fetch initiative data
+  useEffect(() => {
+    /*fetch(`http://localhost:5000/initiative/${id}`, {
 			crossDomain: true,
 			method: 'GET'
 		})
 			.then(response => response.json())
 			.then(data => setInitiative(data));*/
-		setInitiative(data.filter(init => init._id === id)[0]);	
-	}, [id, setInitiative, data])
+    setInitiative(data.filter((init) => init._id === id)[0]);
+  }, [id, setInitiative, data]);
 
-	if (!!initiative) {
-		let { 
-			name, 
-			category, 
-			description,
-			hood, 
-			province,
-			address,
-			activities,
-			contact_phones,
-			emergency,
-			geolocation
-		} = initiative;
+  if (!!initiative) {
+    let {
+      name,
+      category,
+      description,
+      hood,
+      province,
+      address,
+      activities,
+      contact_phones,
+      emergency,
+      geolocation,
+    } = initiative;
 
-		return (
-			<React.Fragment>
-				<div className={`initiative-jumbotron-wrapper${(emergency)?' emergency':''}`}>
-					<Details name={name}
-							 category={category} 
-							 hood={hood} 
-							 province={province}
-							 contact_phones={contact_phones} 
-							 address={address} 
-							 description={description}
-							 emergency={emergency} />
-				</div>
-				<MapContainer geolocation={geolocation} />
-				<Schedule activities={activities} />
-				<div className='aligner'></div>
-			</React.Fragment>
-		)
-	}
-	else {
-		return <div>Cargando...</div>
-	}
-}
+    return (
+      <div className="main-wrapper">
+        <div
+          className={`initiative-jumbotron-wrapper${
+            emergency ? " emergency" : ""
+          }`}
+        >
+          <Details
+            name={name}
+            category={category}
+            hood={hood}
+            province={province}
+            contact_phones={contact_phones}
+            address={address}
+            description={description}
+            emergency={emergency}
+          />
+        </div>
+        <MapContainer geolocation={geolocation} />
+        <Schedule activities={activities} />
+        <div className="aligner"></div>
+      </div>
+    );
+  } else {
+    return <div>Cargando...</div>;
+  }
+};
 
 /* Local components */
 
-const Details = ({ name, category, description, hood, province, address, contact_phones, emergency }) => {
+const Details = ({
+  name,
+  category,
+  description,
+  hood,
+  province,
+  address,
+  contact_phones,
+  emergency,
+}) => {
+  const { categoryDisplay, icon } = getCategoryDisplay(category);
 
-	const { categoryDisplay, icon } = getCategoryDisplay(category);
+  const scrollToSchedule = () => {
+    //const e = document.querySelector('.initiative-schedule');
+    const e = document.getElementById("schedule-title");
+    window.scrollTo({ top: e.offsetTop - 30, behavior: "smooth" });
+  };
 
-	const scrollToSchedule = () => {
-		//const e = document.querySelector('.initiative-schedule');
-		const e = document.getElementById('schedule-title');
-		window.scrollTo({ top: e.offsetTop - 30, behavior: 'smooth' });
-	}
+  const scrollToMap = () => {
+    const e = document.getElementById("map-title");
+    window.scrollTo({ top: e.offsetTop - 30, behavior: "smooth" });
+  };
 
-	const scrollToMap = () => {
-		const e = document.getElementById('map-title');
-		window.scrollTo({ top: e.offsetTop - 30, behavior: 'smooth' });
-	}
+  return (
+    <div className="details">
+      <h1 className="name">{name}</h1>
+      <div className="info-wrapper">
+        <h2 className="icon">{icon}</h2>
+        <h2 className="info">{`${categoryDisplay} - ${hood}, ${province}`}</h2>
+      </div>
 
-	return (
-		<div className='details'>
-			<h1 className='name'>{name}</h1>
-			<div className='info-wrapper'>
-				<h2 className='icon'>{icon}</h2>
-				<h2 className='info'>{`${categoryDisplay} - ${hood}, ${province}`}</h2>
-			</div>
-
-			{/*<p className='description'>{description}</p>*/}
-			<div className='info-wrapper'>
-				<p className='icon'><FaHome /></p>
-				<p className='info'>{address}</p>
-			</div>
-			<div className='info-wrapper'>
-				<p className='icon'><FaPhone /></p>
-				{
-					contact_phones.map((phone, index) => (
-						<React.Fragment>
-							{ (index === 0) ? null : <p className='separator'>-</p> }
-							<p className='info'>{phone}</p>
-						</React.Fragment>
-				 	))
-				}
-			</div>
-			<div className='info-wrapper'>
-				<p className='icon'><FaMapMarkedAlt /></p>
-				<button className='button-link'
-						onClick={() => scrollToMap()}><p>Ver ubiación</p></button>
-			</div>
-			<div className='info-wrapper'>
-				<p className='icon'><FaClock /></p>
-				<button className='button-link'
-						onClick={() => scrollToSchedule()}><p>Ver horarios</p></button>
-			</div>
-			<div className='info-wrapper'>
-				<p className='icon'><FaHandHoldingHeart /></p>
-				<p className='info'>Esta iniciativa recibe donaciones de:</p>
-				{ ['Alimentos','Ropa','Artículos de Higiene Personal'].map((j, ind) => (
-					<p className='donation' key={ind}>{j}</p>
-				)) }
-			</div>
-		</div>
-	)
-}
+      {/*<p className='description'>{description}</p>*/}
+      <div className="info-wrapper">
+        <p className="icon">
+          <FaHome />
+        </p>
+        <p className="info">{address}</p>
+      </div>
+      <div className="info-wrapper">
+        <p className="icon">
+          <FaPhone />
+        </p>
+        {contact_phones.map((phone, index) => (
+          <React.Fragment>
+            {index === 0 ? null : <p className="separator">-</p>}
+            <p className="info">{phone}</p>
+          </React.Fragment>
+        ))}
+      </div>
+      <div className="info-wrapper">
+        <p className="icon">
+          <FaMapMarkedAlt />
+        </p>
+        <button className="button-link" onClick={() => scrollToMap()}>
+          <p>Ver ubiación</p>
+        </button>
+      </div>
+      <div className="info-wrapper">
+        <p className="icon">
+          <FaClock />
+        </p>
+        <button className="button-link" onClick={() => scrollToSchedule()}>
+          <p>Ver horarios</p>
+        </button>
+      </div>
+      <div className="info-wrapper">
+        <p className="icon">
+          <FaHandHoldingHeart />
+        </p>
+        <p className="info">Esta iniciativa recibe donaciones de:</p>
+        {["Alimentos", "Ropa", "Artículos de Higiene Personal"].map(
+          (j, ind) => (
+            <p className="donation" key={ind}>
+              {j}
+            </p>
+          )
+        )}
+      </div>
+    </div>
+  );
+};
 
 //
 
 const MapContainer = ({ geolocation }) => {
-	let { latitude, longitude } = geolocation;
+  let { latitude, longitude } = geolocation;
 
-	return (
-		<React.Fragment>
-			<h1 className='section-title' id='map-title'>Ubicación</h1>
-			<div className='map-wrapper'>
-				<Map center={[latitude, longitude]} zoom={15} scrollWheelZoom={false}>
-			    	<TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-			        		   attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
-			        <Marker key={0}
-					        position={[
-					        	latitude,
-					            longitude
-					        ]}
-					        onClick={() => {}} />
-			    </Map>
-			</div>
-		</React.Fragment>
-	);
-}
+  return (
+    <React.Fragment>
+      <h1 className="section-title" id="map-title">
+        Ubicación
+      </h1>
+      <div className="map-wrapper">
+        <Map center={[latitude, longitude]} zoom={15} scrollWheelZoom={false}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          />
+          <Marker key={0} position={[latitude, longitude]} onClick={() => {}} />
+        </Map>
+      </div>
+    </React.Fragment>
+  );
+};
 
 //
 
 const START_HOUR = 6;
 const END_HOUR = 23;
-const TOTAL_TIME = END_HOUR-START_HOUR+1;
+const TOTAL_TIME = END_HOUR - START_HOUR + 1;
 
-const HOURS = ['06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23'];
+const HOURS = [
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+  "13",
+  "14",
+  "15",
+  "16",
+  "17",
+  "18",
+  "19",
+  "20",
+  "21",
+  "22",
+  "23",
+];
 
 const Schedule = ({ activities }) => {
+  let { sun, mon, tue, wed, thu, fri, sat } = activities;
 
-	let { sun, mon, tue, wed, thu, fri, sat} = activities;
+  return (
+    <React.Fragment>
+      <h1 className="section-title" id="schedule-title">
+        Horarios
+      </h1>
+      <div className="initiative-schedule">
+        <div className="schedule-header">
+          <h4>Hora</h4>
+          {HOURS.map((hour, j) => (
+            <div className="hour" key={j}>
+              {hour}
+            </div>
+          ))}
+        </div>
 
-	return (
-		<React.Fragment>
-			<h1 className='section-title' id='schedule-title'>Horarios</h1>
-			<div className='initiative-schedule'>
-				<div className='schedule-header'>
-					<h4>Hora</h4>
-					{ HOURS.map((hour, j) => ( <div className='hour' key={j}>{hour}</div> ))}
-				</div>
-
-				<DaySchedule day='Domingo'   activities={sun} />
-				<DaySchedule day='Lunes'     activities={mon} />
-				<DaySchedule day='Martes'    activities={tue} />
-				<DaySchedule day='Miércoles' activities={wed} />
-				<DaySchedule day='Jueves'    activities={thu} />
-				<DaySchedule day='Viernes'   activities={fri} />
-				<DaySchedule day='Sábado'    activities={sat} />
-			</div> 
-		</React.Fragment>
-	)
-}
+        <DaySchedule day="Domingo" activities={sun} />
+        <DaySchedule day="Lunes" activities={mon} />
+        <DaySchedule day="Martes" activities={tue} />
+        <DaySchedule day="Miércoles" activities={wed} />
+        <DaySchedule day="Jueves" activities={thu} />
+        <DaySchedule day="Viernes" activities={fri} />
+        <DaySchedule day="Sábado" activities={sat} />
+      </div>
+    </React.Fragment>
+  );
+};
 
 const DaySchedule = ({ day, activities }) => {
+  const toHourFormat = (hour_float) => {
+    let hour = Math.floor(hour_float);
+    let minutes = `${Math.floor((hour_float - hour) * 60)}`;
+    if (hour.length === 1) {
+      hour = `0${hour}`;
+    }
+    if (minutes.length === 1) {
+      minutes = `0${minutes}`;
+    }
 
-	const toHourFormat = (hour_float) => {
-		let hour = Math.floor(hour_float);
-		let minutes = `${Math.floor((hour_float - hour)*60)}`;
-		if(hour.length === 1) { hour = `0${hour}` }
-		if(minutes.length === 1) { minutes = `0${minutes}` };
+    return `${hour}:${minutes}`;
+  };
 
-		return (`${hour}:${minutes}`)
-	} 
-
-	return (
-		<div className='day-schedule-wrapper'>
-			<h4>{day}</h4>
-			<div className='day-schedule'>
-				<div className='hours'>
-					{ HOURS.map((hour, j) => ( <div className='hour' key={j}></div> ))}
-				</div>
-				<div className='activities'>
-					{ 
-						activities.map((activity, index) => {
-							const { start, duration, description } = activity;
-							const left = `calc(${(start-START_HOUR)/TOTAL_TIME*100}% + 2px)`;
-							const width = `calc(${duration/TOTAL_TIME*100}% - 4px)`;
-							return (
-								<div className='activity' key={index} style={{ left: left, width: width }}>
-									<div className='activity-body'></div>
-									<div className='activity-info'>
-										<div className='body'>
-											{/*<p>{description}</p>
+  return (
+    <div className="day-schedule-wrapper">
+      <h4>{day}</h4>
+      <div className="day-schedule">
+        <div className="hours">
+          {HOURS.map((hour, j) => (
+            <div className="hour" key={j}></div>
+          ))}
+        </div>
+        <div className="activities">
+          {activities.map((activity, index) => {
+            const { start, duration, description } = activity;
+            const left = `calc(${
+              ((start - START_HOUR) / TOTAL_TIME) * 100
+            }% + 2px)`;
+            const width = `calc(${(duration / TOTAL_TIME) * 100}% - 4px)`;
+            return (
+              <div
+                className="activity"
+                key={index}
+                style={{ left: left, width: width }}
+              >
+                <div className="activity-body"></div>
+                <div className="activity-info">
+                  <div className="body">
+                    {/*<p>{description}</p>
 											<br />*/}
-											<p>{toHourFormat(start)} - {toHourFormat(start+duration)}</p>
-										</div>
-										<div className='triangle'></div>
-									</div>
-								</div>
-							)
-						})
-					}
-				</div>
-			</div>
-		</div>
-	)
-}
+                    <p>
+                      {toHourFormat(start)} - {toHourFormat(start + duration)}
+                    </p>
+                  </div>
+                  <div className="triangle"></div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default InitiativeView;
--- src/views/SearchResultsView.js
+++ src/views/SearchResultsView.js
@@ -1,162 +1,150 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from "react";
 
-import { 
-	FaExclamation,
-} from 'react-icons/fa';
-
-import { 
-	useLocation,
-	Link
-} from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 
 /* Components */
-import LoadingAnimation from '../components/LoadingAnimation';
+import LoadingAnimation from "../components/LoadingAnimation";
 
 /* Helpers */
-import { getCategoryDisplay } from '../helpers/getCategoryDisplay';
-import { filterByName } from '../helpers/filterByName';
+import { getCategoryDisplay } from "../helpers/getCategoryDisplay";
+import { filterByName } from "../helpers/filterByName";
 
 /* Styles */
-import '../main.scss';
-import '../styles/search-results.scss';
+import "../main.scss";
+import "../styles/search-results.scss";
 
 /* Data hooks */
-import { useData } from '../context/data';
+import { useData } from "../context/data";
 
 const SearchResultsView = () => {
-	const query = new URLSearchParams(useLocation().search);
-	const [search,   setSearch]   = useState(query.get('search'));
-	const [province, setProvince] = useState(undefined);
-	const [hood, 	 setHood]     = useState(undefined);
+  const query = new URLSearchParams(useLocation().search);
+  const [search, setSearch] = useState(query.get("search"));
+  const [province, setProvince] = useState(undefined);
+  const [hood, setHood] = useState(undefined);
 
-	return (
-		<React.Fragment>
-			<SearchBar value={search}
-					   hood={hood}
-					   province={province}
-					   _setValue={(value) => setSearch(value)} />
-			<SearchResults search={search} />
-		</React.Fragment>
-	)
-}
+  return (
+    <Fragment>
+      <SearchBar
+        value={search}
+        hood={hood}
+        province={province}
+        _setValue={(value) => setSearch(value)}
+      />
+      <SearchResults search={search} />
+    </Fragment>
+  );
+};
 
 /* Local components */
 
 const SearchBar = ({ hood, province, value, _setValue }) => {
+  const [options, setOptions] = useState(false);
 
-	const [options, setOptions] = useState(false);
-
-	return (
-		<div className='searchbar'>
-			<input value={value}
-				   placeholder='Buscar por nombre...'
-				   onChange={(e) => _setValue(e.target.value)}/>
-			{(options) ? (
-				<React.Fragment>
-					<div className='advanced-options'>
-						<div className='select-wrapper'>
-							<label>Departamento</label>
-							<select value={province}>
-								<option>Ciudad...</option>
-								<option></option>
-								<option></option>
-							</select>
-						</div>
-						<div className='select-wrapper'>
-							<label>Barrio</label>
-							<select value={hood}>
-								<option>Ciudad...</option>
-								<option></option>
-								<option></option>
-							</select>
-						</div>
-					</div>
-					<button onClick={() => setOptions(false)}>Menos opciones</button>
-				</React.Fragment>
-			):( 
-				<button onClick={() => setOptions(true)}>Opciones avanzadas</button>
-			)}
-		</div>
-	)
-}
+  return (
+    <div className="searchbar">
+      <input
+        value={value}
+        placeholder="Buscar por nombre..."
+        onChange={(e) => _setValue(e.target.value)}
+      />
+      {options ? (
+        <Fragment>
+          <div className="advanced-options">
+            <div className="select-wrapper">
+              <label>Departamento</label>
+              <select value={province}>
+                <option>Ciudad...</option>
+                <option></option>
+                <option></option>
+              </select>
+            </div>
+            <div className="select-wrapper">
+              <label>Barrio</label>
+              <select value={hood}>
+                <option>Ciudad...</option>
+                <option></option>
+                <option></option>
+              </select>
+            </div>
+          </div>
+          <button onClick={() => setOptions(false)}>Menos opciones</button>
+        </Fragment>
+      ) : (
+        <button onClick={() => setOptions(true)}>Opciones avanzadas</button>
+      )}
+    </div>
+  );
+};
 
 //
 
 const SearchResults = ({ search }) => {
-	const [initiatives, setInitiatives] = useState(null);
+  const [initiatives, setInitiatives] = useState(null);
 
-	const { data, isDataFetching } = useData();
-	const [filteredData, setFilteredData] = useState([]);
+  const { data, isDataFetching } = useData();
+  const [filteredData, setFilteredData] = useState([]);
 
-	// Fetch initiative data
-	useEffect(() => {
-		setFilteredData(filterByName(data, search));
-		/*fetch('http://localhost:5000/initiatives/search', {
+  // Fetch initiative data
+  useEffect(() => {
+    setFilteredData(filterByName(data, search));
+    /*fetch('http://localhost:5000/initiatives/search', {
 			crossDomain: true,
 			method: 'GET'
 		})
 			.then(response => response.json())
 			.then(data => setInitiatives(data));*/
-	}, [setFilteredData, data, search]);
+  }, [setFilteredData, data, search]);
 
-	const renderInitiatives = () => {
-		if (!isDataFetching) {
-			return (
-				<div className='results-container'>
-					<div className='results-inner'>
-						<div className='initiatives-container'>
-							{filteredData.slice(0,20).map((init, index) => (
-								<Initiative initiative={init} index={index} key={index} />
-							))}
-						</div>
-					</div>
-				</div>
-			)
-		}
+  const renderInitiatives = () => {
+    if (!isDataFetching) {
+      return (
+        <div className="results-container">
+          <div className="initiatives-container">
+            {filteredData.slice(0, 20).map((init, index) => (
+              <Initiative initiative={init} index={index} key={index} />
+            ))}
+          </div>
+        </div>
+      );
+    } else {
+      return <LoadingAnimation />;
+    }
+  };
 
-		else {
-			return <LoadingAnimation />
-		}
-	}
-
-	return (
-		<React.Fragment>
-			{renderInitiatives()}
-		</React.Fragment>	
-	)
-}
+  return <Fragment>{renderInitiatives()}</Fragment>;
+};
 
 const Initiative = ({ initiative, index }) => {
+  const { _id, name, category, hood, province, emergency } = initiative;
 
-	const { _id, name, category, hood, province, emergency } = initiative;
+  // If emergency is true, use associated class
+  let emergency_class = "";
+  if (emergency) {
+    emergency_class = " emergency";
+  }
 
-	// If emergency is true, use associated class
-	let emergency_class = '';
-	if(emergency) {
-		emergency_class = ' emergency';
-	}
-
-	return (
-		<Link to={`/initiative/${_id}`} className={`initiative-card${emergency_class}`}>
-			<CategoryIcon category={category} emergency_class={emergency_class} />
-			<h4 className='location'>{`${hood},`}<br />{province}</h4>
-			<p className='name'>{name}</p>
-			{ emergency ? <EmergencyNotice /> : null }
-		</Link>
-	);
-}
+  return (
+    <Link
+      to={`/initiative/${_id}`}
+      className={`initiative-card${emergency_class}`}
+    >
+      {emergency ? <span className="emergency-text">EMERGENCIA</span> : null}
+      <CategoryIcon category={category} emergency_class={emergency_class} />
+      <h4 className="location">
+        {`${hood}`}
+        <br />
+        {province}
+      </h4>
+      <p className="name">{name}</p>
+    </Link>
+  );
+};
 
 const CategoryIcon = ({ category, emergency_class }) => {
-	let { icon } = getCategoryDisplay(category);
+  let { icon } = getCategoryDisplay(category);
 
-	return (
-		<p className={`icon${emergency_class}`}>{icon}</p>
-	)
-}
-
-const EmergencyNotice = () => {
-	return <p className='emergency-notice'><FaExclamation /></p>;
-}
+  return <p className={`icon${emergency_class}`}>{icon}</p>;
+};
 
 /* ------------------ */
 
