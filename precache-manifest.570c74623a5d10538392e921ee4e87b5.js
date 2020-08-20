0cef7dea7a06937eaa6e6fcfa05ef2be89320321
Danilo Espino <daniloespino@gmail.com>
2020-08-20 01:31:03 -0300

    Updates

---
+++ precache-manifest.570c74623a5d10538392e921ee4e87b5.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed7b9cdd13f1878012da0b85cda2a02f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "857f4aadcfa6c2a87e0a",
+    "url": "/static/css/main.208e2047.chunk.css"
+  },
+  {
+    "revision": "74326fed959db337c81a",
+    "url": "/static/js/2.ef2c576a.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.ef2c576a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "857f4aadcfa6c2a87e0a",
+    "url": "/static/js/main.ba8e6631.chunk.js"
+  },
+  {
+    "revision": "56ec9ebb628a01b07b70",
+    "url": "/static/js/runtime-main.6e3265cf.js"
+  },
+  {
+    "revision": "7343136420dbfa82e68c5f5bce87b463",
+    "url": "/static/media/CEDA.73431364.jpg"
+  },
+  {
+    "revision": "c2e9e4b6a1ecda85b0837ad376c5f0e2",
+    "url": "/static/media/FEUU.c2e9e4b6.jpg"
+  },
+  {
+    "revision": "438067105a3748550894745b31b21f04",
+    "url": "/static/media/Intersocial Sur.43806710.jpg"
+  },
+  {
+    "revision": "f50bd0089565543f40c56bfcdf320aaf",
+    "url": "/static/media/Red de Ollas al Sur.f50bd008.png"
+  },
+  {
+    "revision": "3e218fa384088e8b96d75a73dec9fd36",
+    "url": "/static/media/adasu.3e218fa3.jpg"
+  },
+  {
+    "revision": "6a13635016d6b01fa1eba78f25b08e43",
+    "url": "/static/media/audyn.6a136350.png"
+  },
+  {
+    "revision": "8d42ba258f7bc09614d41a5fff70c6df",
+    "url": "/static/media/basket.8d42ba25.svg"
+  },
+  {
+    "revision": "76dc47cb1db27ee33ea152f8e5459f5a",
+    "url": "/static/media/cecso.76dc47cb.png"
+  },
+  {
+    "revision": "ca6b7318f927020ddfca9c1b5ad04bce",
+    "url": "/static/media/cooking_pot.ca6b7318.svg"
+  },
+  {
+    "revision": "d68a44e52511ccb3b50a3a3cb031c0ab",
+    "url": "/static/media/donate_home.d68a44e5.svg"
+  },
+  {
+    "revision": "9389faf565aefee9d33d069cf348d7a4",
+    "url": "/static/media/donate_jumbo_home.9389faf5.svg"
+  },
+  {
+    "revision": "6fc54d36552e1b248b6d53fbe3c15eb9",
+    "url": "/static/media/donation_point.6fc54d36.svg"
+  },
+  {
+    "revision": "83294973fbc14b13b96754fa8fb7293c",
+    "url": "/static/media/emergency.83294973.svg"
+  },
+  {
+    "revision": "dffa93eb74bf5b8316509afc49469aed",
+    "url": "/static/media/entre.dffa93eb.png"
+  },
+  {
+    "revision": "c015e6523633aba490520d8142993f18",
+    "url": "/static/media/galponcorrales.c015e652.jpg"
+  },
+  {
+    "revision": "5e40480fefd89c3be8a82b353f814685",
+    "url": "/static/media/homebg.5e40480f.jpg"
+  },
+  {
+    "revision": "67b7934efb49bdc45b63c1e930a49ae7",
+    "url": "/static/media/participate_home.67b7934e.svg"
+  },
+  {
+    "revision": "91dcf246136efe1d20fd45abfae10172",
+    "url": "/static/media/placeholderphoto.91dcf246.jpg"
+  },
+  {
+    "revision": "5cef95709c6d0251fa3c67efff673ba0",
+    "url": "/static/media/promote_home.5cef9570.svg"
+  },
+  {
+    "revision": "1eba90e4ce652f1bab97bcb8d2ed8e55",
+    "url": "/static/media/radio pedal.1eba90e4.png"
+  },
+  {
+    "revision": "c22a57cdc8b606612d39fb8e83cdef10",
+    "url": "/static/media/redoeste.c22a57cd.jpg"
+  },
+  {
+    "revision": "e0c5b3888e5e42be097e3dd6c0c8762d",
+    "url": "/static/media/smu.e0c5b388.png"
+  },
+  {
+    "revision": "aa8b289ab5e1fcd7d52be6aa9edc5425",
+    "url": "/static/media/tea_time.aa8b289a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/solidaridaduy-react/precache-manifest.43871facc490cf6f5d1a24065035f1b3.js"
+  "/precache-manifest.570c74623a5d10538392e921ee4e87b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/solidaridaduy-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
